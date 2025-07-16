1803aed8f0b9a6016f1660540efa5bf0a088a3f1
May <abeillee@bk.ru>
2025-07-16 17:05:40 +0300

    test: add tests for Card and Description

---
+++ rs-react-app/src/__mocks__/mocks.ts
@@ -0,0 +1,12 @@
+import type { PokemonData } from '@/utils/types';
+
+export const cardData: PokemonData = {
+  name: 'pidgeot',
+  height: 15,
+  weight: 395,
+  abilities: ['keen-eye', 'tangled-feet', 'big-pecks'],
+  sprites: {
+    homefrontDefault:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/18.png',
+  },
+};
