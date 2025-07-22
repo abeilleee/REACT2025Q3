a95463de95f0cf7860940e41fcda8bc965555719
May <abeillee@bk.ru>
2025-07-22 15:41:04 +0300

    refactor: convert Layout to FC

--- rs-react-app/src/components/layouts/Layout/Layout.tsx
+++ rs-react-app/src/components/layouts/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Component, type ReactNode } from 'react';
+import { type FC, type ReactNode } from 'react';
 import styles from './Layout.module.scss';
 import { Footer, Header } from '@/components/ui';
 
@@ -6,19 +6,14 @@ type LayoutProps = {
   children: ReactNode;
 };
 
-export class Layout extends Component<LayoutProps> {
-  constructor(props: LayoutProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={styles.container} data-testid={'layout-container'}>
-        <Header />
-        <main className={styles.content} data-testid={'layout-main'}>
-          {this.props.children}
-        </main>
-        <Footer />
-      </div>
-    );
-  }
-}
+export const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className={styles.container} data-testid={'layout-container'}>
+      <Header />
+      <main className={styles.content} data-testid={'layout-main'}>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
