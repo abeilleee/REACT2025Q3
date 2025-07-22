d286f58df00e0ffa803e5f0f342555a29b2f72b9
May <abeillee@bk.ru>
2025-07-22 15:23:04 +0300

    refactor: convert Card to FC

--- rs-react-app/src/components/ui/Card/Card.tsx
+++ rs-react-app/src/components/ui/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { type FC } from 'react';
 import styles from './Card.module.scss';
 import type { PokemonData } from '@/utils/types';
 import { Description } from '@/components/ui';
@@ -7,26 +7,21 @@ type CardProps = {
   pokemon: PokemonData;
 };
 
-export class Card extends Component<CardProps> {
-  render() {
-    return (
-      <div className={styles.card}>
-        <div className={styles.content}>
-          <div className={styles.name}>{this.props.pokemon.name}</div>
-          <div className={styles['img-box']}>
-            <img
-              src={this.props.pokemon.sprites?.homefrontDefault}
-              alt={this.props.pokemon.name}
-            />
-          </div>
-
-          <Description
-            height={this.props.pokemon.height}
-            weight={this.props.pokemon.weight}
-            abilities={this.props.pokemon.abilities}
-          />
+export const Card: FC<CardProps> = ({ pokemon }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.content}>
+        <div className={styles.name}>{pokemon.name}</div>
+        <div className={styles['img-box']}>
+          <img src={pokemon.sprites?.homefrontDefault} alt={pokemon.name} />
         </div>
+
+        <Description
+          height={pokemon.height}
+          weight={pokemon.weight}
+          abilities={pokemon.abilities}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
