1a8b2bdffedc5c958d4593e2426082da8df51595
May <abeillee@bk.ru>
2025-07-24 01:56:13 +0300

    feat: implement Not Found page

--- rs-react-app/src/pages/NotFound/NotFound.tsx
+++ rs-react-app/src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,24 @@
+import img from '@/assets/images/not-found.png';
 import type { FC } from 'react';
+import styles from './NotFound.module.scss';
+import { Button } from '@/components/ui';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from '@/services/router/constants';
 
 export const NotFound: FC = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate(PATHS.ROOT);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.text}>Oops! Page not found...</p>
+      <div className={styles['img-box']}>
+        <img src={img} alt="pikachu" height="256px" />
+      </div>
+      <Button onClick={onClick} textContent="Back to main"></Button>
+    </div>
+  );
 };
