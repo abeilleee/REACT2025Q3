4e6fe43dbc51f5387db23ebd102c3547625077dd
May <abeillee@bk.ru>
2025-07-23 22:55:17 +0300

    feat: replace Error Button with About Button

--- rs-react-app/src/components/ui/Header/Header.tsx
+++ rs-react-app/src/components/ui/Header/Header.tsx
@@ -1,15 +1,23 @@
 import { type FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '@/assets/images/logo.png';
-import { ErrorButton } from '@/components/common';
+import { PATHS } from '@/services/router/constants';
 import styles from './Header.module.scss';
+import { Button } from '../Button';
 
 export const Header: FC = () => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate(PATHS.ABOUT);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
       </div>
-      <ErrorButton />
+      <Button onClick={onClick} textContent="About"></Button>
     </header>
   );
 };
--- rs-react-app/src/pages/About/About.tsx
+++ rs-react-app/src/pages/About/About.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@/components/ui';
+import { PATHS } from '@/services/router/constants';
 import type { FC } from 'react';
 
 export const About: FC = () => {
-  return <div>About</div>;
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate(PATHS.ROOT);
+  };
+  return (
+    <>
+      <div>About</div>
+      <Button onClick={onClick} textContent="Back to main"></Button>
+    </>
+  );
 };
