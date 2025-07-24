a7da873fe1d7d2998f41fdcddce996f07cf29387
May <abeillee@bk.ru>
2025-07-24 03:40:15 +0300

    test: add test for Header

--- rs-react-app/src/components/ui/Header/Header.test.tsx
+++ rs-react-app/src/components/ui/Header/Header.test.tsx
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Header } from './Header';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { navigateMock } from '@/__tests__/setupTests';
+import { PATHS } from '@/services/router/constants';
 
 describe('Header test', () => {
+  beforeAll(() => {
+    vi.mock('react-router-dom', async () => {
+      return {
+        ...(await vi.importActual('react-router-dom')),
+        useNavigate: () => navigateMock,
+      };
+    });
+  });
+
   test('should render Header logo and button', () => {
     render(
       <MemoryRouter>
@@ -15,5 +27,20 @@ describe('Header test', () => {
 
     expect(logo).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  test('should call onClick and navigate to about page when button clicked', async () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    const user = userEvent.setup();
+    const button = screen.getByText('About');
+
+    await user.click(button);
+    await waitFor(() => expect(navigateMock).toHaveBeenCalledTimes(1));
+    expect(navigateMock).toHaveBeenCalledWith(PATHS.ABOUT);
   });
 });
