6b6bbf7f9aff71b4c146724bd627948cca9db4c5
May <abeillee@bk.ru>
2025-07-24 03:24:43 +0300

    test: add tests for Not Found Page

--- rs-react-app/src/__tests__/setupTests.ts
+++ rs-react-app/src/__tests__/setupTests.ts
@@ -1,8 +1,17 @@
 import { server } from '@/__mocks__/msw/server';
 import '@testing-library/jest-dom';
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
+export const navigateMock = vi.fn();
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  vi.clearAllMocks();
+});
+
 afterAll(() => {
   server.close();
   localStorage.clear();
--- rs-react-app/src/pages/NotFound/NotFound.tsx
+++ rs-react-app/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,9 @@
-import img from '@/assets/images/not-found.png';
-import type { FC } from 'react';
-import styles from './NotFound.module.scss';
-import { Button } from '@/components/ui';
 import { useNavigate } from 'react-router-dom';
+import img from '@/assets/images/not-found.png';
+import { Button } from '@/components/ui';
 import { PATHS } from '@/services/router/constants';
+import styles from './NotFound.module.scss';
+import type { FC } from 'react';
 
 export const NotFound: FC = () => {
   const navigate = useNavigate();
@@ -16,7 +16,12 @@ export const NotFound: FC = () => {
     <div className={styles.wrapper}>
       <p className={styles.text}>Oops! Page not found...</p>
       <div className={styles['img-box']}>
-        <img src={img} alt="pikachu" height="256px" />
+        <img
+          src={img}
+          alt="pikachu"
+          height="256px"
+          data-testId={'img-not-found'}
+        />
       </div>
       <Button onClick={onClick} textContent="Back to main"></Button>
     </div>
